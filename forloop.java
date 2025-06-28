d502651b13af7aac42304b567a933519e46e405c
SHUBHAM <shubhamagrahari9191@gmail.com>
2025-06-29 02:35:51 +0530

    add remote

---
+++ forloop.java
@@ -0,0 +1,14 @@
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, World! " + i);
+        }
+    }
+    public static void printMessage() {
+        System.out.println("This is a message from the forloop class.");
+    }
+    public static void anotherMethod() {
+        System.out.println("This is another method in the forloop class.");
+    } 
+
+}
